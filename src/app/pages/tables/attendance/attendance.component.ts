aefee257973150afc8a35cde2d49e6e9396a8af7
msc-dts <naphol159@gmail.com>
2020-05-15 09:06:31 +0700

    utf8

--- src/app/pages/tables/attendance/attendance.component.ts
+++ src/app/pages/tables/attendance/attendance.component.ts
@@ -37,105 +37,24 @@ export class AttendanceComponent implements OnInit {
       this.http.get<any[]>('http://20.188.110.129:3000/attendance/' + data.id).subscribe((attendance) => {
 
         attendance.forEach((element) => {
-          let emotion1 = element.checkin.emotion.emotion;
-          //console.log("emo:", emotion1);
-          let emo = 'neutral';
-          let maxProp = null
-          let maxValue = -1
-          let secProp = null
-          let secValue = -1
+          this.http.get<any[]>('http://20.188.110.129:3000/getcropimage/' + element.checkinImageCrop).subscribe((image) => {
+            element['image1'] = 'data:image/jpg;base64,' + image['data'];
+            if (element.checkoutdatetime! = '') {
+              this.http.get<any[]>('http://20.188.110.129:3000/getcropimage/' + element.checkoutImageCrop).subscribe((image2) => {
+                element['image2'] = 'data:image/jpg;base64,' + image2['data'];
 
-          for (var prop in emotion1) {
-            if (emotion1.hasOwnProperty(prop)) {
 
-              let value = emotion1[prop]
-              if (value > maxValue) {
-                secValue = maxValue;
-                secProp = maxProp
-                maxValue = value;
-                maxProp = prop
-              } else if (secValue < value) {
-                secValue = value;
-                secProp = prop
-              }
+
+              })
             }
-          }
 
-          if (maxProp == 'neutral') {
-            if (maxValue == 1) {
-            }
-            else if (maxValue < 1) {
-              if (secProp == 'anger') emo = 'anger';
-              else if (secProp == 'contempt') emo = 'contempt';
-              else if (secProp == 'disgust') emo = 'disgust';
-              else if (secProp == 'fear') emo = 'fear';
-              else if (secProp == 'happiness') emo = 'happiness';
-              else if (secProp == 'sadness') emo = 'sadness';
-              else if (secProp == 'surprise') emo = 'surprise';
-            }
-          }
-          else if (maxProp == 'anger') emo = 'anger';
-          else if (maxProp == 'contempt') emo = 'contempt';
-          else if (maxProp == 'disgust') emo = 'disgust';
-          else if (maxProp == 'fear') emo = 'fear';
-          else if (maxProp == 'happiness') emo = 'happiness';
-          else if (maxProp == 'sadness') emo = 'sadness';
-          else if (maxProp == 'surprise') emo = 'surprise';
 
-          element['emotion1'] = emo;
+          })
+
+
+          
         })
-
-        attendance.forEach((element) => {
-          if (element.checkout.time != '') {
-            let emotion1 = element.checkout.emotion.emotion;
-            //console.log("emo:", emotion1);
-
-            let maxProp = null
-            let maxValue = -1
-            let secProp = null
-            let secValue = -1
-            let emo = "neutral";
-            for (var prop in emotion1) {
-              if (emotion1.hasOwnProperty(prop)) {
-
-                let value = emotion1[prop]
-                if (value > maxValue) {
-                  secValue = maxValue;
-                  secProp = maxProp
-                  maxValue = value;
-                  maxProp = prop
-                } else if (secValue < value) {
-                  secValue = value;
-                  secProp = prop
-                }
-              }
-            }
-
-            if (maxProp == 'neutral') {
-              if (maxValue == 1) {
-              }
-              else if (maxValue < 1) {
-                if (secProp == 'anger') emo = 'anger';
-                else if (secProp == 'contempt') emo = 'contempt';
-                else if (secProp == 'disgust') emo = 'disgust';
-                else if (secProp == 'fear') emo = 'fear';
-                else if (secProp == 'happiness') emo = 'happiness';
-                else if (secProp == 'sadness') emo = 'sadness';
-                else if (secProp == 'surprise') emo = 'surprise';
-              }
-            }
-            else if (maxProp == 'anger') emo = 'anger';
-            else if (maxProp == 'contempt') emo = 'contempt';
-            else if (maxProp == 'disgust') emo = 'disgust';
-            else if (maxProp == 'fear') emo = 'fear';
-            else if (maxProp == 'happiness') emo = 'happiness';
-            else if (maxProp == 'sadness') emo = 'sadness';
-            else if (maxProp == 'surprise') emo = 'surprise';
-
-            element['emotion2'] = emo;
-          }
-        })
-        this.dataSource = attendance
+        this.dataSource = attendance;
       })
     })
   }
