952d23d3ed37ad4aec7cc15326d53f2ea5976c63
msc-dts <naphol159@gmail.com>
2020-06-24 08:03:15 +0700

    a

--- src/app/pages/facedetection/info/info.component.ts
+++ src/app/pages/facedetection/info/info.component.ts
@@ -103,7 +103,7 @@ export class InfoComponent implements OnInit {
                 element['nameem'] = pr.name;
                 element['surname'] = pr.surname;
                 element['per'] = "(" + (element.confidence * 100).toFixed(2) + "%)";
-                element['individual_confidence'] = (element.individual_confidence * 100).toFixed(2);
+                element['individual_confidence'] = (pr.individual_confidence * 100).toFixed(2);
               }
 
             })
@@ -305,7 +305,7 @@ export class InfoComponent implements OnInit {
                 element['nameem'] = pr.name;
                 element['surname'] = pr.surname;
                 element['per'] = "(" + (element.confidence * 100).toFixed(2) + "%)";
-                element['individual_confidence'] = (element.individual_confidence * 100).toFixed(2);
+                element['individual_confidence'] = (pr.individual_confidence * 100).toFixed(2);
 
               }
 
