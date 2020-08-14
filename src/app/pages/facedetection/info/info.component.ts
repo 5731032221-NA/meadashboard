9977d934f0f693b7326ca1b91178f95a725e82aa
msc-dts <naphol159@gmail.com>
2020-08-14 16:57:41 +0700

    a

--- src/app/pages/facedetection/info/info.component.ts
+++ src/app/pages/facedetection/info/info.component.ts
@@ -59,7 +59,11 @@ export class InfoComponent implements OnInit {
     this.http.get<any[]>('http://20.188.110.129:3000/getcropinfobydate/' + querydate).subscribe((cropinfo) => {
 
       this.http.get<any[]>('http://20.188.110.129:3000/getmeaprofile').subscribe(profile => {
-
+        if (cropinfo.length > 0) {
+          this.empty = false;
+        } else {
+          this.empty = true;
+        }
 
 
         cropinfo.forEach((element) => {
@@ -133,11 +137,11 @@ export class InfoComponent implements OnInit {
           })
         }
         this.dataSource = cropinfo;
-        if (this.dataSource.length > 0) {
-          this.empty = false;
-        } else {
-          this.empty = true;
-        }
+        // if (this.dataSource.length > 0) {
+        //   this.empty = false;
+        // } else {
+        //   this.empty = true;
+        // }
         // console.log("aa", this.dataSource);
         this.spinner.hide();
       })
@@ -149,12 +153,58 @@ export class InfoComponent implements OnInit {
 
   }
 
-  trainDialog(item, name, title, nameem, surname, rowid): void {
-    // console.log(item.item)
+  test(item, name, title, nameem, surname, rowid,datetime,date,camera): void {
+    console.log("item",item)
+    var e = document.getElementById(item)  as HTMLSelectElement;;
+var strUser = e.options[e.selectedIndex].value;
+    console.log(strUser)
+    // // console.log(item.item)
+    // let id = item.substring(0, 7);
+    // const dialogRef = this.dialog.open(TrainComponent, {
+    //   width: '820px',
+    //   data: { id, name, title, item, surname, rowid ,datetime,date,camera}
+    // });
+    // dialogRef.afterClosed().subscribe(result => {
+
+    //   if (result) {
+
+    //     for (var i = 0; i < this.dataSource.length; i++) {
+    //       if (this.dataSource[i]._id === rowid) {
+    //         this.dataSource[i].train = id;
+    //         this.http.get<any[]>('http://20.188.110.129:3000/getmeaprofile').subscribe(profile => {
+
+
+    //           // console.log("train")
+    //           profile.forEach((pr) => {
+    //             if (id == pr.id) {
+    //               this.dataSource[i]['ttitle'] = pr.title;
+    //               this.dataSource[i]['tnameem'] = pr.name;
+    //               this.dataSource[i]['tsurname'] = pr.surname;
+    //             }
+
+    //           })
+
+
+
+    //         })
+    //         break;
+    //       }
+    //     }
+    //   }
+     
+
+    // });
+  }
+
+
+  trainDialog( name, title, nameem, surname, rowid,datetime,date,camera): void {
+    // console.log(rowid)
+    let e = document.getElementById(rowid)  as HTMLSelectElement;;
+    let item = e.options[e.selectedIndex].value;
     let id = item.substring(0, 7);
     const dialogRef = this.dialog.open(TrainComponent, {
       width: '820px',
-      data: { id, name, title, item, surname, rowid }
+      data: { id, name, title, item, surname, rowid ,datetime,date,camera}
     });
     dialogRef.afterClosed().subscribe(result => {
 
@@ -183,75 +233,7 @@ export class InfoComponent implements OnInit {
           }
         }
       }
-      // let date = this.model;
-
-      // let date_ob = new Date(date.year, date.month - 1, date.day);
-
-      // let day = ("0" + date_ob.getDate()).slice(-2);
-
-      // // current month
-      // let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-
-      // // current year
-      // let year = date_ob.getFullYear();
-
-      // var querydate = year + "-" + ("0" + month).slice(-2) + "-" + ("0" + day).slice(-2);
-
-      // this.http.get<any[]>('http://20.188.110.129:3000/getcropinfobydate/' + querydate).subscribe((cropinfo) => {
-
-      //   this.http.get<any[]>('http://20.188.110.129:3000/getmeaprofile').subscribe(profile => {
-
-      //     // profile.sort((a, b) => (b.name - a.name));
-
-
-      //     cropinfo.forEach((element) => {
-
-
-      //       if (element["train"] != "") {
-      //         console.log("train")
-      //         profile.forEach((pr) => {
-      //           if (element.train == pr.id) {
-      //             element['ttitle'] = pr.title;
-      //             element['tnameem'] = pr.name;
-      //             element['tsurname'] = pr.surname;
-      //           }
-
-      //         })
-
-      //         element['canselect'] = false;
-      //       }
-      //       else element['canselect'] = false;
-
-      //       this.http.get<any[]>('http://20.188.110.129:3000/getcropimage/' + element.name).subscribe((image) => {
-
-      //         element['image1'] = 'data:image/jpg;base64,' + image['data'];
-
-
-      //       })
-
-      //       if (element.detected != "") {
-      //         profile.forEach((pr) => {
-      //           if (element.detected == pr.id) {
-      //             element['title'] = pr.title;
-      //             element['nameem'] = pr.name;
-      //             element['surname'] = pr.surname;
-      //             element['per'] = "(" + (element.confidence * 100).toFixed(2) + "%)";
-      //           }
-
-      //         })
-      //       }
-      //     })
-
-
-
-      //     let list = [{ 'name': "เลือกพนักงาน -" }, ...profile];
-      //     list.sort((a, b) => (a.id - b.id));
-      //     this.listmea = list;
-      //     this.dataSource = cropinfo;
-      //     // console.log("aa", this.dataSource);
-      //     this.spinner.hide();
-      //   })
-      // })
+     
 
     });
   }
@@ -276,7 +258,12 @@ export class InfoComponent implements OnInit {
       this.http.get<any[]>('http://20.188.110.129:3000/getmeaprofile').subscribe(profile => {
 
         // profile.sort((a, b) => (b.id - a.id));
-
+        if (cropinfo.length > 0) {
+          this.empty = false;
+        } else {
+          this.empty = true;
+          this.dataSource = [];
+        }
         cropinfo.forEach((element) => {
 
           if (element["train"] != "") {
@@ -345,11 +332,11 @@ export class InfoComponent implements OnInit {
           })
         }
         this.dataSource = cropinfo;
-        if (this.dataSource.length > 0) {
-          this.empty = false;
-        } else {
-          this.empty = true;
-        }
+        // if (this.dataSource.length > 0) {
+        //   this.empty = false;
+        // } else {
+        //   this.empty = true;
+        // }
         // console.log("aa", this.dataSource);
         this.spinner.hide();
       })
