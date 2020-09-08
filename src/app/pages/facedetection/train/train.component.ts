55a8c77ae9f8ffd4aaa72679dbae30dc915e6309
msc-dts <naphol159@gmail.com>
2020-09-08 15:45:07 +0700

    a

--- src/app/pages/facedetection/train/train.component.ts
+++ src/app/pages/facedetection/train/train.component.ts
@@ -62,10 +62,11 @@ export class TrainComponent {
 
   onSubmit() {
    
-    this.http.get<any[]>('http://20.188.110.129:3000/traincropimage/' + this.data.name + '/' + this.data.id + '/' + this.data.rowid).subscribe((done) => {
+   
       // if (this.data.camera == 2 || this.data.camera == 4) {
         this.http.get<any[]>('http://20.188.110.129:3000/chagnetime/' + this.data.date + '/' + this.data.datetime + '/' + this.data.id + '/' + this.data.name).subscribe((done) => {
-          this.dialogRef.close(true)
+          this.http.get<any[]>('http://20.188.110.129:3000/traincropimage/' + this.data.name + '/' + this.data.id + '/' + this.data.rowid).subscribe((done) => {
+        this.dialogRef.close(true)
         })
       // }  
       // else {
